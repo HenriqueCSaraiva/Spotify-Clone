e513dcb1aab1462f697f486bd84294d9c53c1ed3
Henrique Saraiva <henriquesaraiva24@gmail.com>
2021-11-29 07:49:36 -0300

    Add album option on footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,16 +9,30 @@ import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import { Grid, Slider } from "@material-ui/core";
+import { useStateProviderValue } from "../../StateProvider";
 
 function Footer() {
+  const [{ token, item, playing }, dispatch] = useStateProviderValue();
+
   return (
     <div className="footer">
       <div className="footer__left">
-        <img className="footer__albumLogo" src="" alt="" />
-        <div className="footer__songInfo">
-          <h4>Yeah!</h4>
-          <p>Usher</p>
-        </div>
+        <img
+          className="footer__albumLogo"
+          src={item?.album.images[0].url}
+          alt={item?.name}
+        />
+        {item ? (
+          <div className="footer__songInfo">
+            <h4>{item.name}</h4>
+            <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+          </div>
+        ) : (
+          <div className="footer__songInfo">
+            <h4>No song is playing</h4>
+            <p>...</p>
+          </div>
+        )}
       </div>
       <div className="footer__center">
         <ShuffleIcon className="footer__green" />
